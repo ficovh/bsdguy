ef221a4d44b83d150955a3dc060bc6551d106db4
ficovh <ficovh@gmail.com>
2016-02-14 21:19:32 -0600

    add about page

--- pelicanconf.py
+++ pelicanconf.py
@@ -18,8 +18,8 @@ TRANSLATION_FEED_ATOM = None
 BANNER = 'images/banner.jpg'
 
 PROFILE_IMG_URL = u'http://www.gravatar.com/avatar/ab1e128805502b007dd29966ca93273c.png'
-STATIC_PATHS = ['images']
-
+STATIC_PATHS = ['images', 'pages']
+DISPLAY_PAGES_ON_MENU = 'True'
 
 # Blogroll
 LINKS =  (('Pelican', 'http://getpelican.com/'),
